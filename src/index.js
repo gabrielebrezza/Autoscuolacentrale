76046d453d98900b81f323bb43b5edca73e28c6c
gabriele <brezzagabriele0@gmail.com>
2024-03-16 12:41:20 +0100

    .

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const collection = require('./User');
+const cookieParser = require('cookie-parser');
+
+// Aggiungi cookie-parser come middleware per gestire i cookie
+
+
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(express.json());
 
@@ -40,32 +47,45 @@ app.post("/signup", async (req, res) =>{
     }
 });
 
-app.post('/login', async (req, res) =>{
 
-    try{
-        const check = await collection.findOne({userName: req.body.username});
-        if(!check){
-            res.send('this username does not exist');
-        }
-
-        //check the password
-        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
-        if(isPasswordMatch){
-            
-            console.log('nuovo utente loggato: ', req.body.username);
-            res.redirect(`/profile/:${req.body.username}`);
-        }else{
-            res.send('password errata');
-        }
-    }catch{
-        res.send('Credenziali sbagliate ');
+// Middleware per controllare l'autenticazione
+const isAuthenticated = (req, res, next) => {
+    const usernameCookie = req.cookies.username;
+    if (usernameCookie) {
+        // L'utente è autenticato
+        return next();
+    } else {
+        // L'utente non è autenticato, reindirizzalo alla pagina di login
+        res.redirect('/');
     }
-})
+};
 
-app.get('/profile/:username', async (req, res)=>{
+app.post('/login', async (req, res) => {
+    try {
+        const check = await collection.findOne({ userName: req.body.username });
+        if (!check) {
+            res.send('This username does not exist');
+        }
 
+        // Check the password
+        const sixMonths = 180 * 24 * 60 * 60 * 1000;
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if (isPasswordMatch) {
+            // Imposta il cookie di autenticazione
+            res.cookie('username', req.body.username, { maxAge: 60000, httpOnly: true });
+            console.log('Nuovo utente loggato: ', req.body.username);
+            res.redirect(`/profile/:${req.body.username}`);
+        } else {
+            res.send('Password errata');
+        }
+    } catch {
+        res.send('Credenziali sbagliate');
+    }
+});
+
+app.get('/profile/:username', isAuthenticated, async (req, res) => {
     const nome = req.params.username;
-    res.send(`benvenuto ${nome}`);
+    res.send(`Benvenuto ${nome}`);
 });
 
 const port = 5000;
