1a95d2000a1ee78169e6486b8ae8297054187aaf
gabriele <brezzagabriele0@gmail.com>
2024-04-07 12:21:40 +0200

    .

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 
 app.use(express.static('public'));
 
