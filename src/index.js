c0814252d4d148d880fc770181a6a8e5016b7567
gabriele <brezzagabriele0@gmail.com>
2024-04-14 12:59:41 +0200

    .

--- src/index.js
+++ src/index.js
@@ -706,7 +706,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello World!\n');
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
