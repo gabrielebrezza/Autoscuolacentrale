5fb48699ca0dbb932fcecc2af7dddb4f85a3c195
gabriele <brezzagabriele0@gmail.com>
2024-04-14 13:04:56 +0200

    .

--- src/index.js
+++ src/index.js
@@ -703,7 +703,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n');
+  res.redirect('/');
 });
 
 const PORT = process.env.PORT || 80;
