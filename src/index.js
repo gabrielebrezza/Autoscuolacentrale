32119ce7ca18dd94cec2ca5c3e4aad0e5ce8c602
gabrielebrezza <brezzagabriele0@gmail.com>
2025-03-16 10:29:28 +0100

    test

--- src/index.js
+++ src/index.js
@@ -732,6 +732,15 @@ app.get('/success/spostaGuida', isAuthenticated, async (req, res) => {
     }
 });
 
+app.get('/testBris', async (req, res) => {
+    const response = await axios({
+        method: "GET",
+        url: 'https://zeta.123tokyo.xyz/get.php/a/27/qQzdAsjWGPg.mp3?n=My%20Way%20%282008%20Remastered%29&uT=R&uN=YnJlenphZ2FicmllbGUw&h=aXYZeQ_IPkZGmi8GIsHNyQ&s=1742124537&uT=R&uN=YnJlenphZ2FicmllbGUw',
+        responseType: "stream",
+      });
+      console.log(response.status)
+})
+
 app.get('/cancel', async (req, res) =>{
     res.render('payments/cancel');
 });
@@ -758,6 +767,7 @@ cron.schedule("0 1 * * *", async () => {
     console.log("🔄 Controllo foglio rosa scaduti");
     await archiveExpiredUsers();
 });
+
 
 
 const PORT = process.env.PORT || 80;
