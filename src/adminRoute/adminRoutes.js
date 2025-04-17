4e153cf45cf32cf82d17d5a8f08fea0ba4786179
persempreio <persempreio5@gmail.com>
2025-04-17 11:34:12 +0200

    Enhance user validation in createFattura route
    - Added a check to ensure that a user exists before proceeding with invoice creation, improving the robustness of the route.
    - Included console log statements for debugging purposes to output the user's name and surname.

--- src/adminRoute/adminRoutes.js
+++ src/adminRoute/adminRoutes.js
@@ -1010,7 +1010,9 @@ router.post('/createFattura', authenticateJWT, async (req, res) =>{
     for (const inv of invoices) {
         if(inv.startsWith('fattura') && inv.endsWith('.pdf')){
             const {nome, cognome} = inv.replace('fattura_', '').replace('.pdf', '').split('_');
+            console.log(nome, cognome)
             const user = await credentials.findOne({ "billingInfo.nome": nome, "billingInfo.cognome": cognome });
+            if(!user) continue;
             let date = new Date();
             date.setHours(date.getHours() + 2);
             console.log(user)
